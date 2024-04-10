301507539b0aa354ff9aaead604a0aa49e89c848
sbx_user1051 <sbx_user1051@169.254.43.29>
2024-04-10 19:53:04 +0000

    Wed Apr 10 19:53:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,27 @@
+import dataclasses
+from typing import Optional
 
+
+@dataclasses.dataclass
+class Offers:
+    quantity: int
+    total_price: int
+
+@dataclasses.dataclass
+class Item:
+    price: int
+    offers: Optional[Offers] = None
+
+PRICE_CONFIGS = {
+    "A": Item(price=50, offers=Offers(quantity=3, total_price=130)),
+    "B": Item(price=30, offers=Offers(quantity=2, total_price=45)),
+    "C": Item(price=20),
+    "D": Item(price=15),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    list_of_items = skus.split()
+    if 
+
